053884adb5575e764318a360095865cdf3c9d3b8
Ivona Skrinjar <i.skrinjar@levi9.com>
2022-12-13 14:51:08 +0100

    updated shipping details

--- src/components/CheckoutPage/ReviewOrder/ShippingDetails.tsx
+++ src/components/CheckoutPage/ReviewOrder/ShippingDetails.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Typography, Grid } from "@material-ui/core";
-import useStyles from "./styles";
 
 function PaymentDetails(props: any) {
     const { formValues } = props;
-    const classes = useStyles();
     const { firstName, lastName, address } = formValues;
     return (
         <Grid item xs={12} sm={6}>
-            <Typography variant="h6" gutterBottom className={classes.title}>
+            <Typography variant="h6" gutterBottom >
         Shipping
             </Typography>
             <Typography gutterBottom>{`${firstName} ${lastName}`}</Typography>
