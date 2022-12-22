3ceb5d511987c4277f1bcc4b5ea05b5eea9c0a7e
Ivona Skrinjar <i.skrinjar@levi9.com>
2022-12-22 10:09:42 +0100

    update index file

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ProductProvider } from "context/product/ProductProvider";
 import { FilterProvider } from "context/filter/FilterProvider";
 import { CartProvider } from "context/cart/CartProvider";
@@ -15,19 +15,19 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <AuthProvider>
             <ProductProvider>
                 <FilterProvider>
                     <CartProvider>
-                        <WishlistProvider>            
+                        <WishlistProvider>
                             <App />
                         </WishlistProvider>
                     </CartProvider>
                 </FilterProvider>
             </ProductProvider>
         </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
