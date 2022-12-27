1d5769dcf99b873faf8e7f612555cfa52c16703b
Ivona Skrinjar <i.skrinjar@levi9.com>
2022-12-27 15:34:28 +0100

    optimized the styles

--- src/components/FilterSection/FilterSection.tsx
+++ src/components/FilterSection/FilterSection.tsx
@@ -4,162 +4,162 @@ import React, { useContext, useState } from "react";
 import { FilterContext } from "context/filter/FilterContext";
 import { ProductContext } from "context/product/ProductContext";
 import {
-    Autocomplete,
-    Box,
-    Button,
-    Checkbox,
-    FormControlLabel,
-    Slider,
-    TextField,
-    Typography,
+  Autocomplete,
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Slider,
+  TextField,
+  Typography,
 } from "@mui/material";
 import { IProduct } from "interfaces";
 import { useTranslation } from "react-i18next";
 
 const FilterSection = () => {
-    const { categories, products } = useContext(ProductContext);
-    const [value, setValue] = useState<IProduct | null>(null);
-    const [checked] = useState([true, false]);
-    const [inputValue, setInputValue] = useState("");
-    const [sliderValue, setSliderValue] = useState(null); 
+  const { categories, products } = useContext(ProductContext);
+  const [value, setValue] = useState<IProduct | null>(null);
+  const [checked] = useState([true, false]);
+  const [inputValue, setInputValue] = useState("");
+  const [sliderValue, setSliderValue] = useState(null); 
    
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const {
-        filters: { category, price, maxPrice, minPrice },
-        updateFilterValue,
-        clearFilters,
-    } = useContext(FilterContext);
+  const {
+    filters: { category, price, maxPrice, minPrice },
+    updateFilterValue,
+    clearFilters,
+  } = useContext(FilterContext);
 
 
 
-    const categoryData = ["all", ...categories];
+  const categoryData = ["all", ...categories];
    
-    const handleChanges = async (event: any) => {
-        setValue(value);
-        updateFilterValue(event);
-    };
+  const handleChanges = async (event: any) => {
+    setValue(value);
+    updateFilterValue(event);
+  };
 
-    const handleClearFilters = () => {
-        setValue(null); 
-        setInputValue("");
-        clearFilters();
-    };
+  const handleClearFilters = () => {
+    setValue(null); 
+    setInputValue("");
+    clearFilters();
+  };
 
-    const defaultProps = {
-        options: products,
-        getOptionLabel: (option: IProduct) => option.title,
-    };
-    return (
-        <Wrapper>
-            <Autocomplete
-                {...defaultProps}
-                value={value}
-                onChange={(e, value) => {
-                    setValue(value);
-                    updateFilterValue({
-                        target: { name: "text", value: value },
-                    });
-                }}
-                inputValue={inputValue}
-                onInputChange={(e, v) => {
-                    setInputValue(v);
-                }}
-                renderInput={(params) => (
-                    <TextField
-                        {...params}
-                        label={
-                            <Typography sx={{ fontSize: "13px", color: "grey" }}>
-                                {t("search")}
-                            </Typography>
-                        }
-                        variant="outlined"
-                        fullWidth
-                    />
-                )}
-            />
+  const defaultProps = {
+    options: products,
+    getOptionLabel: (option: IProduct) => option.title,
+  };
+  return (
+    <Wrapper>
+      <Autocomplete
+        {...defaultProps}
+        value={value}
+        onChange={(e, value) => {
+          setValue(value);
+          updateFilterValue({
+            target: { name: "text", value: value },
+          });
+        }}
+        inputValue={inputValue}
+        onInputChange={(e, v) => {
+          setInputValue(v);
+        }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label={
+              <Typography sx={{ fontSize: "13px", color: "grey" }}>
+                {t("search")}
+              </Typography>
+            }
+            variant="outlined"
+            fullWidth
+          />
+        )}
+      />
 
-            <div className="filter_price">
-                <Typography variant="h4" component="h4">
-                    {t("filter-price")}
-                </Typography>
-                <Typography
-                    sx={{
-                        fontSize: "1.5rem",
-                        float: "right",
-                        marginTop: "3rem",
-                    }}
-                >
-                    {numberFormatCurrency.formatNumber(price)}
-                </Typography>
-                <Slider
-                    sx={{
-                        maxWidth: "33rem",
-                    }}
-                    defaultValue={maxPrice}
-                    step={10}
-                    min={minPrice}
-                    max={maxPrice}
-                    value={price}
-                    onChange={(e, value: any) => {
-                        setSliderValue(value);
-                    }}
-                    onChangeCommitted={(e, value) => {
-                        updateFilterValue({
-                            target: { name: "price", value: value },
+      <div className="filter_price">
+        <Typography variant="h4" component="h4">
+          {t("filter-price")}
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "1.5rem",
+            float: "right",
+            marginTop: "3rem",
+          }}
+        >
+          {numberFormatCurrency.formatNumber(price)}
+        </Typography>
+        <Slider
+          sx={{
+            maxWidth: "33rem",
+          }}
+          defaultValue={maxPrice}
+          step={10}
+          min={minPrice}
+          max={maxPrice}
+          value={price}
+          onChange={(e, value: any) => {
+            setSliderValue(value);
+          }}
+          onChangeCommitted={(e, value) => {
+            updateFilterValue({
+              target: { name: "price", value: value },
+            });
+          }}
+        />
+      </div>
+      <div className="filter-category">
+        <Typography variant="h4" component="h4">
+          {t("filter-category")}
+        </Typography>
+        <Box>
+          <div className="category-button">
+            {categoryData.map((c, index) => {
+              return (
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={category === c.toLowerCase() ? true : false}
+                      onChange={() => {
+                        handleChanges({
+                          target: {
+                            name: "category",
+                            checked: checked,
+                            value: c,
+                          },
                         });
-                    }}
-                />
-            </div>
-            <div className="filter-category">
-                <Typography variant="h4" component="h4">
-                    {t("filter-category")}
-                </Typography>
-                <Box>
-                    <div className="category-button">
-                        {categoryData.map((c, index) => {
-                            return (
-                                <FormControlLabel
-                                    control={
-                                        <Checkbox
-                                            checked={category === c.toLowerCase() ? true : false}
-                                            onChange={() => {
-                                                handleChanges({
-                                                    target: {
-                                                        name: "category",
-                                                        checked: checked,
-                                                        value: c,
-                                                    },
-                                                });
-                                            }}
-                                        />
-                                    }
-                                    label={
-                                        <Typography fontSize="1.5rem" color="grey">
-                                            {c}
-                                        </Typography>
-                                    }
-                                    key={index}
-                                />
-                            );
-                        })}
-                    </div>
-                </Box>
-            </div>
-
-            <div className="filter-clear">
-                <Button
-                    variant="contained"
-                    className="clear-button"
-                    onClick={handleClearFilters}
-                >
-                    <Typography fontSize="1.5rem" color="white">
-                        {t("clear-filters")}
+                      }}
+                    />
+                  }
+                  label={
+                    <Typography fontSize="1.5rem" color="grey">
+                      {c}
                     </Typography>
-                </Button>
-            </div>
-        </Wrapper>
-    );
+                  }
+                  key={index}
+                />
+              );
+            })}
+          </div>
+        </Box>
+      </div>
+
+      <div className="filter-clear">
+        <Button
+          variant="contained"
+          className="clear-button"
+          onClick={handleClearFilters}
+        >
+          <Typography fontSize="1.5rem" color="white">
+            {t("clear-filters")}
+          </Typography>
+        </Button>
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
